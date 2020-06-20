3cf41b4013df50f25327da01237a2e932ea53da7
m1nnh <alsgur9784@Naver.com>
2020-06-20 22:00:00 +0900

    Add : 2021-02-17

---
+++ BOJ/BOJ-10757.py
@@ -0,0 +1,11 @@
+"""
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : 큰 수
+description : 수학, 구현, 사칙연산, 임의 정밀도 / 큰 수 연
+"""
+
+A, B = map(int, input().split())
+print(A + B)
