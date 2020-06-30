f89ccef9415973e84532a65d155bb046873cc67d
m1nnh <alsgur9784@Naver.com>
2020-06-30 22:00:00 +0900

    Add : 2021-02-17

---
+++ BOJ/BOJ-5086.py
@@ -0,0 +1,20 @@
+"""
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : 배수와 약
+description : 수학, 구현
+"""
+
+a, b = map(int, input().split())
+
+while a!=0 and b!=0:
+    if b%a==0:
+        print('factor')
+    elif a%b==0:
+        print('multiple')
+    else:
+        print('neither')
+    a, b = map(int, input().split())
+
