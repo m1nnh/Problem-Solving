97f44ac229fb806a4aa3a298eb8b2d33f2bcb483
m1nnh <alsgur9784@Naver.com>
2020-07-02 22:00:00 +0900

    Add : 2021-02-17

---
+++ BOJ/BOJ-4101.py
@@ -0,0 +1,17 @@
+"""
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : 크냐?
+description : 수학, 구현, 사칙연
+"""
+
+a, b = map(int, input().split())
+
+while a!=0 and b!=0 :
+    if a>b:
+        print("Yes")
+    else:
+        print("No")
+    a, b = map(int, input().split())
