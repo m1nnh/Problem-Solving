3a4fd06487674760c3f9112438e89200fad19355
m1nnh <alsgur9784@Naver.com>
2020-07-04 22:00:00 +0900

    Add : 2021-02-17

---
+++ BOJ/BOJ-2754.py
@@ -0,0 +1,37 @@
+"""
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : 학점계산
+description : 수학, 구현, 사칙연산
+"""
+
+c = input()
+
+if c=='A+':
+    print(4.3)
+elif c=='A0':
+    print(4.0)
+elif c=='A-':
+    print(3.7)
+elif c=='B+':
+    print(3.3)
+elif c=='B0':
+    print(3.0)
+elif c=='B-':
+    print(2.7)
+elif c=='C+':
+    print(2.3)
+elif c=='C0':
+    print(2.0)
+elif c=='C-':
+    print(1.7)
+elif c=='D+':
+    print(1.3)
+elif c=='D0':
+    print(1.0)
+elif c=='D-':
+    print(0.7)
+else:
+    print(0.0)
