6bf2890c23e76b27bab4152b2df2e70ee83d15e2
m1nnh <alsgur9784@Naver.com>
2020-07-12 22:00:00 +0900

    Add : 2021-02-16

---
+++ BOJ/BOJ-2935.py
@@ -0,0 +1,17 @@
+"""
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : Noise
+description : Math
+"""
+
+a = int(input())
+b = input()
+c = int(input())
+
+if b == '*':
+    print(a * c)
+else:
+    print(a + c)
