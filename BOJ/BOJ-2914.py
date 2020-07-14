23009537daf6b7a11aa4b95cbe7d0ff70ccb9285
m1nnh <alsgur9784@Naver.com>
2020-07-14 22:00:00 +0900

    Add : 2021-02-16

---
+++ BOJ/BOJ-2914.py
@@ -0,0 +1,11 @@
+"""
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : Literary property
+description : Math
+"""
+
+a, b = map(int, input().split())
+print((a * (b - 1)) + 1)
