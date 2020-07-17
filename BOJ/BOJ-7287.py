0ad3abb7098e1ef5ce65930f0d01f180adb1f6c2
m1nnh <alsgur9784@Naver.com>
2020-07-17 22:00:00 +0900

    Add : 2021-02-16

---
+++ BOJ/BOJ-7287.py
@@ -0,0 +1,11 @@
+"""
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : Registration
+description : Implementation
+"""
+
+print('44')
+print('alsgur9784')
