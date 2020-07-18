400f0ca032b22da6e71e6101ad49b5196c8cc1f5
m1nnh <alsgur9784@Naver.com>
2020-07-18 22:00:00 +0900

    Add : 2021-02-16

---
+++ BOJ/BOJ-10699.py
@@ -0,0 +1,13 @@
+"""
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : Today's date
+description : Implementation
+"""
+
+import datetime
+
+now = datetime.datetime.now()
+print(now.strftime('%Y-%m-%d'))
