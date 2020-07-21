bcd0b77ddd933b81ab6c5db986c57059f76a7284
m1nnh <alsgur9784@Naver.com>
2020-07-21 22:00:00 +0900

    Add : 2021-02-16

---
+++ BOJ/BOJ-2163.py
@@ -0,0 +1,12 @@
+"""
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : Cutting Chocolate
+description : Math
+"""
+
+n, m = map(int, input().split())
+print((n*m)-1)
+
