9ccecaece7dd08d7d6d84c175d6a06652edca145
m1nnh <alsgur9784@Naver.com>
2020-07-22 22:00:00 +0900

    Add : 2021-02-16

---
+++ BOJ/BOJ-3046.py
@@ -0,0 +1,11 @@
+"""
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : R2
+description : Math
+"""
+
+r1, s = map(int, input().split())
+print((s * 2) - r1)
