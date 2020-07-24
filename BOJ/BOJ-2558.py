90e3e819c9688bf4df49c1d961a5766ca0f41a5e
m1nnh <alsgur9784@Naver.com>
2020-07-24 22:00:00 +0900

    Add : 2021-02-16

---
+++ BOJ/BOJ-2558.py
@@ -0,0 +1,13 @@
+"""
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : A+B - 2
+description : Math
+"""
+
+a = int(input())
+b = int(input())
+
+print(a + b)
