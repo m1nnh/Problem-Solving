4c177968e575f85b8c2fabed31b5c51b696b2fae
m1nnh <alsgur9784@Naver.com>
2020-08-18 22:00:00 +0900

    Add : Code up basic 100 problem with Python

---
+++ CodeUp/[071-077]기초-반복실행구조/073.py
@@ -0,0 +1,13 @@
+""" 정수가 순서대로 입력된다.(단, 개수는 알 수 없다.)
+    0이 아니면 입력 된 정수를 출력하고, 0이 입력되면 출력을 중단해보자.
+    정수가 순서대로 입력 된다.
+    입력 : 7 4 2 3 0 1 5 6 9 10 8 """
+
+boolean=True
+
+while boolean == True :
+    num = int(input())
+    if num == 0 :
+        boolean = False
+    else :
+        continue
