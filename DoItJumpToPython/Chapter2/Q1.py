668120cf2fbfe7db7237823ef2cc816d42919569
m1nnh <alsgur9784@Naver.com>
2020-10-29 22:00:00 +0900

    Add : Solved Chapter

---
+++ DoItJumpToPython/Chapter2/Q1.py
@@ -0,0 +1,7 @@
+## 홍길동 씨의 과목별 점수는 다음과 같다. 홍길동 씨의 평균 점수를 구해보자.
+## 국어 : 80, 영어 : 75, 수학 : 55
+
+hong= {'korea':80, 'english':75, 'math':55}
+avg=(hong['korea']+hong['english']+hong['math'])/len(hong)
+print(avg)
+
