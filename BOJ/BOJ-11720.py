a8e19382c0dd6558cf70b9fcd3c87bfa1170e4ac
m1nnh <alsgur9784@Naver.com>
2020-10-30 22:00:00 +0900

    Add : 2021-02-16

---
+++ BOJ/BOJ-11720.py
@@ -0,0 +1,15 @@
+"""
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : Sum
+description : Math
+"""
+
+n=int(input())
+m=input()
+sum=0
+for i in m:
+    sum +=int(i)
+print(sum)
