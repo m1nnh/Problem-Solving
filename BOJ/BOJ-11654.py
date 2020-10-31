77319abcb6aed758df40c9b902e16080246057e7
m1nnh <alsgur9784@Naver.com>
2020-10-31 22:00:00 +0900

    Add : 2021-02-16

---
+++ BOJ/BOJ-11654.py
@@ -0,0 +1,13 @@
+"""
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : ASCII code
+description : Implementation
+"""
+
+a=input()
+
+print(ord(a))
+
