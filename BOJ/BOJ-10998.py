25fd83c62c927d0a9c489176929e34c27a543413
m1nnh <alsgur9784@Naver.com>
2020-11-01 22:00:00 +0900

    Add : 2021-02-16

---
+++ BOJ/BOJ-10998.py
@@ -0,0 +1,13 @@
+"""
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : A x B
+description : Math
+"""
+
+a, b = map(int,input().split())
+
+print(a*b)
+
