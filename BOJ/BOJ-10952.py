8665bd2a3b0cff6c8f24409c29c4c4d224b52ae6
m1nnh <alsgur9784@Naver.com>
2020-11-02 22:00:00 +0900

    Add : 2021-02-16

---
+++ BOJ/BOJ-10952.py
@@ -0,0 +1,14 @@
+"""
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : A+B -5
+description : Math
+"""
+
+while True:
+    a, b=map(int, input().split())
+    if a==0 and b==0:
+        break
+    print(a+b)
