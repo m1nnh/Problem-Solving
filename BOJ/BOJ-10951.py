894e09ebbbc0428d04ecd3562dd3692b9d8689dc
m1nnh <alsgur9784@Naver.com>
2020-11-03 22:00:00 +0900

    Add : 2021-02-16

---
+++ BOJ/BOJ-10951.py
@@ -0,0 +1,15 @@
+"""
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : A+B -4
+description : Math
+"""
+
+while True:
+    try:
+        a, b=map(int,input().split())
+        print(a+b)
+    except:
+        break
