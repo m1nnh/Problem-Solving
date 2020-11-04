425e05d83e4681cb133940e171aba98c83e867d8
m1nnh <alsgur9784@Naver.com>
2020-11-04 22:00:00 +0900

    Add : 2021-02-16

---
+++ BOJ/BOJ-10950.py
@@ -0,0 +1,13 @@
+"""
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : A+B -3
+description : Math
+"""
+
+t = int(input())
+for i in range(t):
+    a, b = map(int, input().split())
+    print(a+b)
