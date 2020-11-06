462fe10fa87f3823450006b96ac3f9edd0aa79f9
m1nnh <alsgur9784@Naver.com>
2020-11-06 22:00:00 +0900

    Add : 2021-02-16

---
+++ BOJ/BOJ-10869.py
@@ -0,0 +1,16 @@
+"""
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : Four Calculation
+description : Math
+"""
+
+A, B = map(int, input().split())
+
+print(A+B)
+print(A-B)
+print(A*B)
+print(A//B)
+print(A%B)
