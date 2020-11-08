9f855b413105d45d5e75e72405f3f9b04d7bd100
m1nnh <alsgur9784@Naver.com>
2020-11-08 22:00:00 +0900

    Add : 2021-02-16

---
+++ BOJ/BOJ-10809.py
@@ -0,0 +1,20 @@
+"""
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : Find alphabet
+description : Implementation
+"""
+
+s = input()
+
+alphabet = [i for i in range(ord('a'),ord('z')+1)]
+
+for i in range(len(alphabet)):
+    if chr(alphabet[i]) in s:
+        idx=s.index(chr(alphabet[i]))
+        alphabet[i]=idx
+    else:
+        alphabet[i]=-1
+    print(alphabet[i], end=' ')
