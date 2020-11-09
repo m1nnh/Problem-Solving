8f80dfefac8ec9cd953bc2515094995939910083
m1nnh <alsgur9784@Naver.com>
2020-11-09 22:00:00 +0900

    Add : 2021-02-16

---
+++ BOJ/BOJ-10172.py
@@ -0,0 +1,14 @@
+"""
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : Dog
+description : Implementation
+"""
+
+print("|\\_/|")
+print("|q p|   /}")
+print('( 0 )"""\\')
+print('|"^"`    |')
+print('||_/=\\\\__|')
