993a2f7be9c5e0ce75aa49d5521b84e90bd80deb
m1nnh <alsgur9784@Naver.com>
2020-11-10 22:00:00 +0900

    Add : 2021-02-16

---
+++ BOJ/BOJ-10171.py
@@ -0,0 +1,13 @@
+"""
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : Cat
+description : Implementation
+"""
+
+print('\\    /\\')
+print(" )  ( ')")
+print("(  /  )")
+print(" \\(__)|")
