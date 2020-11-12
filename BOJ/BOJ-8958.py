4f9725b6d8a39380e1cadcc56d0259b410979b6b
m1nnh <alsgur9784@Naver.com>
2020-11-12 22:00:00 +0900

    Add : 2021-02-16

---
+++ BOJ/BOJ-8958.py
@@ -0,0 +1,22 @@
+"""
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : O / X Quiz
+description : Implementation
+"""
+
+test_num = int(input())
+
+for i in range(test_num):
+    score, count = 0, 0
+    string = input()
+
+    for ox in string :
+        if ox=='O':
+            count += 1
+        else:
+            count = 0
+        score += count
+    print(score)
