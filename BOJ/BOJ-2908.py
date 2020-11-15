e674e74ee93b683d258c62baa7f428dc249df304
m1nnh <alsgur9784@Naver.com>
2020-11-15 22:00:00 +0900

    Add : 2021-02-16

---
+++ BOJ/BOJ-2908.py
@@ -0,0 +1,18 @@
+"""
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : Constants
+description : Implementation
+"""
+
+a, b = map(str, input().split())
+
+a=a[::-1]
+b=b[::-1]
+
+if int(a)>int(b) :
+    print(a)
+else :
+    print(b)
