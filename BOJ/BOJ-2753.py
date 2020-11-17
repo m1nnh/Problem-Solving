8f99f394a46645ae6ac9c7420369b4e01c9a0b13
m1nnh <alsgur9784@Naver.com>
2020-11-17 22:00:00 +0900

    Add : 2021-02-16

---
+++ BOJ/BOJ-2753.py
@@ -0,0 +1,16 @@
+"""
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : Leap year
+description : Math
+"""
+
+year = int(input())
+
+if (year%4==0 and year%100 != 0) or year%400==0:
+    print(1)
+else:
+    print(0)
+
