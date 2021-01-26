ca410d54708e430e906641a6ff2e239f89cce170
m1nnh <alsgur9784@Naver.com>
2021-01-26 22:00:00 +0900

    Add : 2021-02-15

---
+++ BOJ/BOJ-2741.py
@@ -0,0 +1,13 @@
+"""
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : Take n
+description : Implementation
+"""
+
+n = int(input())
+
+for i in range(1,n+1):
+    print(i)
