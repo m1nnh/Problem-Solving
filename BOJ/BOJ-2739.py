0fc8d44724641f1362140f36891fd3d05b333bb8
m1nnh <alsgur9784@Naver.com>
2021-01-27 22:00:00 +0900

    Add : 2021-02-15

---
+++ BOJ/BOJ-2739.py
@@ -0,0 +1,13 @@
+"""
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : Multiplication table
+description : Math
+"""
+
+n = int(input())
+
+for i in range(1,10) :
+    print("{} * {} = {}".format(n,i,n*i))
