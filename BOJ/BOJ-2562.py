b5a6e5b9159e7eb8249845623a6ab4749edd6e05
m1nnh <alsgur9784@Naver.com>
2021-01-31 22:00:00 +0900

    Add : 2021-02-15

---
+++ BOJ/BOJ-2562.py
@@ -0,0 +1,16 @@
+"""
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : Max Value
+description : Implementation
+"""
+
+list_num = []
+
+for i in range(9) :
+    list_num.append(int(input()))
+
+print(max(list_num))
+print(list_num.index(max(list_num))+1)
