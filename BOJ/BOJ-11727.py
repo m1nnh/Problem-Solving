bb8b3658425ca966a2538e899d41346a9fbe49f0
m1nnh <alsgur9784@Naver.com>
2021-02-03 22:00:00 +0900

    Add : 2021-03-11

--- BOJ/BOJ-11727.py
+++ BOJ/BOJ-11727.py
@@ -10,7 +10,7 @@ description : 다이나믹 프로그래밍
 N = int(input())
 dp = [0] * (1001)
 dp[1] = 1
-dp[2] = 2
+dp[2] = 3
 for i in range(3, N + 1):
-    dp[i] = (dp[i - 2] + dp[i - 1]) % 10007
+    dp[i] = (2 * dp[i - 2] + dp[i - 1]) % 10007
 print(dp[N])
