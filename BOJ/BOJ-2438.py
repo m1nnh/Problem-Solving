7439723a4298f99bdbd6417a3aa3f74d8a9d14e7
m1nnh <alsgur9784@Naver.com>
2021-02-04 22:00:00 +0900

    Add : 2021-02-15

---
+++ BOJ/BOJ-2438.py
@@ -0,0 +1,16 @@
+"""
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : Shooting Stars
+description : String Implementation
+"""
+
+n = int(input())
+
+for i in range(n) :
+    print('*'*(i+1))
+
+
+
