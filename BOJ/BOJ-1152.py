5b082fea06d471b131dce28f6cd92c7f90cbc783
m1nnh <alsgur9784@Naver.com>
2021-02-08 22:00:00 +0900

    Add : 2021-02-15

---
+++ BOJ/BOJ-1152.py
@@ -0,0 +1,12 @@
+"""
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : Number of words
+description : String
+"""
+
+String = list(input().split())
+
+print(len(String))
