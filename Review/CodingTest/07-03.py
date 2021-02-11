a1f365395ab39a68352e61136c357d76dd889803
m1nnh <alsgur9784@Naver.com>
2021-02-11 22:00:00 +0900

    Add : 2021-03-07

--- Review/CodingTest/07-03.py
+++ Review/CodingTest/07-03.py
@@ -3,32 +3,30 @@ author : Park Min Hyeok
 github : https://github.com/m1nnh
 e-mail : alsgur9784@naver.com
 
-title : 재귀함수를 이용한 이진탐색
+title : 반복문을 이용한 이진탐색
 description : 이진탐색
 
 """
 
 
 def binary_search(array, start, end, M):
-    if start > end:
-        return None
-    mid = (start + end) // 2
-
-    if array[mid] < M:
-        return binary_search(array, mid + 1, end, M)
-    elif array[mid] > M:
-        return binary_search(array, start, mid - 1, M)
-    else:
-        return mid
+    while start <= end:
+        mid = (start + end) // 2
+        if array[mid] == M:
+            return mid
+        elif array[mid] > M:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
 
 
 N, M = map(int, input().split())
-
 array = list(map(int, input().split()))
 
 result = binary_search(array, 0, N - 1, M)
 
 if result is None:
-    print('none')
+    print('None')
 else:
     print(result + 1)
