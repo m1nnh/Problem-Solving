dab1ed37e54c4709e43a68b087f20097ac1eff31
m1nnh <alsgur9784@Naver.com>
2021-02-13 22:00:00 +0900

    Add : 2021-02-15

---
+++ BOJ/BOJ-1000.py
@@ -0,0 +1,12 @@
+"""
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : A+B
+description : Four rule calculations
+"""
+
+a, b = map(int, input().split())
+
+print(a+b)
