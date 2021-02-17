6b898e3d93016cca1f08bc1b59f9a7a6218c9a93
m1nnh <alsgur9784@Naver.com>
2021-02-18 02:46:52 +0900

    Add : 2021-02-18

---
+++ BOJ/BOJ-1436.py
@@ -0,0 +1,18 @@
+"""
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : 영화감독 숌
+description : 브루트포스 알고리즘
+"""
+
+n = int(input())
+
+mv = 666
+
+while n:
+    if '666' in str(mv):
+        n -= 1
+    mv += 1
+print(mv - 1)
