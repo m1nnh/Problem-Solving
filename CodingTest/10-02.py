2225962d9f683538c33d34bfe75d335e53d4b58f
m1nnh <alsgur9784@Naver.com>
2021-02-26 22:00:00 +0900

    Add : 2021-02-28

--- CodingTest/10-02.py
+++ CodingTest/10-02.py
@@ -9,5 +9,5 @@ title : 경로 압축 기법
 
 def find_parent(parent, x):
     if parent[x] != x:
-        return find_parent(parent, parent[x])
+        parent[x] = find_parent(parent, parent[x])
     return parent[x]
