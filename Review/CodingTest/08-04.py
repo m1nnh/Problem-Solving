99ba90a54439911360877c7199abceed07b7bce8
m1nnh <alsgur9784@Naver.com>
2021-03-08 00:34:07 +0900

    Add : 2021-03-08

---
+++ Review/CodingTest/08-04.py
@@ -0,0 +1,22 @@
+"""
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : 1로 만들기
+description : 다이나믹 프로그래밍
+
+"""
+
+N = int(input())
+dp = [0] * 30001
+
+for i in range(2, N + 1):
+    dp[i] = dp[i - 1] + 1
+
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i // 2] + 1)
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i // 3] + 1)
+    if i % 5 == 0:
+        dp[i] = min(dp[i], dp[i // 5] + 1)
+print(dp[N])
