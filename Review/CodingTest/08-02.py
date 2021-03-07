6f1078d165bca946e59abd3e7d7beed089e1e644
m1nnh <alsgur9784@Naver.com>
2021-03-08 00:21:49 +0900

    Add : 2021-03-08

---
+++ Review/CodingTest/08-02.py
@@ -0,0 +1,22 @@
+"""
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : 피보나치 함수 소스코드
+description : 다이나믹 프로그래밍
+
+"""
+
+
+def fibo(x):
+    if x == 1 or x == 2:
+        return 1
+    if d[x] != 0:
+        return d[x]
+    d[x] = fibo(x - 1) + fibo(x - 2)
+    return d[x]
+
+
+d = [0] * 100
+
+print(fibo(99))
