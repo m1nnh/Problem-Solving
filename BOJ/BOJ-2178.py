5daa0c7fff50943473766ffa89071a2b0c2ea0bc
m1nnh <alsgur9784@Naver.com>
2021-03-09 02:48:15 +0900

    Add : 2021-03-09

---
+++ BOJ/BOJ-2178.py
@@ -0,0 +1,50 @@
+"""
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+
+title : 미로 탐색
+N×M 크기의 배열로 표현되는 미로가 있다.
+
+```
+1	0	1	1	1	1
+1	0	1	0	1	0
+1	0	1	0	1	1
+1	1	1	0	1	1
+```
+
+미로에서 1은 이동할 수 있는 칸을 나타내고, 0은 이동할 수 없는 칸을 나타낸다. 이러한 미로가 주어졌을 때, (1, 1)에서 출발하여 (N, M)의 위치로 이동할 때 지나야 하는 최소의 칸 수를 구하는 프로그램을 작성하시오. 한 칸에서 다른 칸으로 이동할 때, 서로 인접한 칸으로만 이동할 수 있다.
+
+위의 예에서는 15칸을 지나야 (N, M)의 위치로 이동할 수 있다. 칸을 셀 때에는 시작 위치와 도착 위치도 포함한다.
+
+description : DFS/BFS
+"""
+
+from collections import deque
+
+
+def BFS(graph):
+    queue = deque()
+    queue.append((0, 0))
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = dx[i] + x
+            ny = dy[i] + y
+            if 0 <= nx < N and 0 <= ny < M:
+                if graph[nx][ny] == 1:
+                    queue.append((nx, ny))
+                    graph[nx][ny] += graph[x][y]
+
+
+N, M = map(int, input().split())
+graph = []
+
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+for i in range(N):
+    graph.append(list(map(int, input())))
+
+BFS(graph)
+print(graph[-1][-1])
