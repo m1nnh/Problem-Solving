fac8975d7e98a1ac144503ec1a1cf5200556b8f8
m1nnh <alsgur9784@Naver.com>
2021-03-16 22:00:00 +0900

    Add : 2021-03-28

---
+++ BOJ/BOJ-1865.py
@@ -0,0 +1,43 @@
+"""
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : 웜홀
+description :
+"""
+
+
+def Bellman_Ford(N, distance, graph):
+    distance[1] = 0
+
+    for check in range(N):
+        for vertex in range(1, N + 1):
+            for next_cost, next_vertex in graph[vertex]:
+                if distance[next_vertex] > distance[vertex] + next_cost:
+                    distance[next_vertex] = distance[vertex] + next_cost
+                    if check == N - 1:
+                        return False
+    return True
+
+
+TC = int(input())
+
+for _ in range(TC):
+    N, M, W = map(int, input().split())
+    graph = [[] for _ in range(N + 1)]
+    distance = [int(1e9)] * (N + 1)
+
+    for _ in range(M):
+        node_a, node_b, cost = map(int, input().split())
+        graph[node_a].append((cost, node_b))
+        graph[node_b].append((cost, node_a))
+
+    for _ in range(W):
+        node_start, node_end, cost = map(int, input().split())
+        graph[node_start].append((-cost, node_end))
+
+    if Bellman_Ford(N, distance, graph):
+        print('NO')
+    else:
+        print('YES')
