4ecee151b02b1a8205d7bae381fc88566a6e7b2b
m1nnh <alsgur9784@Naver.com>
2021-03-18 22:00:00 +0900

    Add : 2021-03-31

---
+++ Java/SpecialCharEx.java
@@ -0,0 +1,20 @@
+/*
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : Java Jungsuk Ex 02 - 08
+description : Char
+*/
+
+package JavaJungsuk;
+
+public class SpecialCharEx {
+    public static void main(String[] args) {
+        System.out.println('\'');
+        System.out.println("abc\t123\b456");
+        System.out.println('\n');
+        System.out.println("\"Hello\"");
+        System.out.println("c:\\");
+    }
+}
