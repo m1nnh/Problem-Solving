720b23a3ff4a1d23d7f3a39d5498741597524096
m1nnh <alsgur9784@Naver.com>
2021-03-19 22:00:00 +0900

    Add : 2021-03-31

---
+++ Java/PrintfEx2.java
@@ -0,0 +1,35 @@
+/*
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : Java Jungsuk Ex 02 - 05
+description : printf()
+*/
+
+package JavaJungsuk;
+
+public class PrintfEx2 {
+    public static void main(String [] args) {
+        String url = "www.github.com/m1nnh";
+
+        float f1 = .10f;
+        float f2 = 1e1f;
+        float f3 = 3.14e3f;
+        double d = 1.23456789;
+
+        System.out.printf("f1=%f, %e, %g%n", f1, f1, f1);
+        System.out.printf("f2=%f, %e, %g%n", f2, f2, f2);
+        System.out.printf("f3=%f, %e, %g%n", f3, f3, f3);
+
+        System.out.printf("d=%f\n", d);
+        System.out.printf("d=%14.10f%n", d);
+
+        System.out.printf("[12345678901234567890]%n");
+        System.out.printf("[%s]%n", url);
+        System.out.printf("[%20s]%n", url);
+        System.out.printf("[%-20s]%n", url);
+        System.out.printf("[%.8s]%n", url);
+
+    }
+}
