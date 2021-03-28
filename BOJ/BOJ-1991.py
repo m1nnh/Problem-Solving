a7d68e08560d6ccdf43e9e0c20568ce8397a979d
m1nnh <alsgur9784@Naver.com>
2021-03-29 00:09:59 +0900

    Add : 2021-03-29

--- BOJ/BOJ-1991.py
+++ BOJ/BOJ-1991.py
@@ -4,5 +4,50 @@ github : https://github.com/m1nnh
 e-mail : alsgur9784@naver.com
 
 title : 트리 순회
-description : 
+description : 재귀함수
 """
+
+
+def pre(now_node):
+    if now_node == '.':
+        return
+    else:
+        print(now_node, end='')
+        left, right = Tree[now_node]
+        pre(left)
+        pre(right)
+
+
+def mid(now_node):
+    if now_node == '.':
+        return
+    else:
+        left, right = Tree[now_node]
+        mid(left)
+        print(now_node, end='')
+        mid(right)
+
+
+def post(now_node):
+    if now_node == '.':
+        return
+    else:
+        left, right = Tree[now_node]
+        post(left)
+        post(right)
+        print(now_node, end='')
+
+
+N = int(input())
+
+Tree = {}
+
+for _ in range(N):
+    parent, left_child, right_child = map(str, input().split())
+    Tree[parent] = [left_child, right_child]
+
+pre('A')
+print()
+mid('A')
+print()
+post('A')
