d8fb58075602036b526cace93713e7c96ab0f0c5
m1nnh <alsgur9784@Naver.com>
2021-03-29 02:05:59 +0900

    Add : 2021-03-29

---
+++ BOJ/BOJ-11404.py
@@ -0,0 +1,36 @@
+"""
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : 플로이드
+description : 플로이드 와샬
+"""
+
+N = int(input())
+M = int(input())
+
+distance = [[int(1e9)] * (N + 1) for _ in range(N + 1)]
+
+for i in range(1, N + 1):
+    for j in range(1, N + 1):
+        if i == j:
+            distance[i][j] = 0
+
+for _ in range(M):
+    node_start, node_end, node_cost = map(int, input().split())
+    if distance[node_start][node_end] > node_cost:
+        distance[node_start][node_end] = node_cost
+
+for k in range(1, N + 1):
+    for i in range(1, N + 1):
+        for j in range(1, N + 1):
+            distance[i][j] = min(distance[i][j], distance[k][j] + distance[i][k])
+
+for i in range(1, N + 1):
+    for j in range(1, N + 1):
+        if distance[i][j] >= int(1e9):
+            print(0, end=' ')
+        else:
+            print(distance[i][j], end=' ')
+    print()
