28d7c7ea1ed7306454f0e06b576938e5e71b9bd8
m1nnh <alsgur9784@Naver.com>
2021-03-28 18:10:40 +0900

    Add : 2021-03-28

---
+++ BOJ/BOJ-1629.py
@@ -0,0 +1,11 @@
+"""
+author : Park Min Hyeok
+github : https://github.com/m1nnh
+e-mail : alsgur9784@naver.com
+
+title : 곱셈
+description : pow함수
+"""
+
+A, B, C = map(int, input().split())
+print(pow(A, B, C))
